ce706a8fdc13c6654860eb01d5f12c79119ebed1
Anisetus  Bambang Manalu <anisetusm@gmail.com>
2025-01-04 11:49:44 +0700

    update

---
+++ queue.h
@@ -0,0 +1,30 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#define MAX_QUEUE_SIZE 50
+
+typedef struct {
+    int id_penumpang;
+    char nama_penumpang[50];
+    char jenis_antrian[20];
+    char waktu_kedatangan[20];
+    char status[20];
+} Penumpang;
+
+typedef struct {
+    Penumpang data[MAX_QUEUE_SIZE];
+    int front, rear;
+} Queue;
+
+// Deklarasi fungsi
+void initQueue(Queue *q);
+int isEmpty(Queue *q);
+int isFull(Queue *q);
+int isIdExists(Queue *q, int id);
+int isValidTime(const char *time);
+void addQueue(Queue *q, Penumpang p);
+Penumpang delQueue(Queue *q);
+Penumpang peek(Queue *q);
+void peekAll(Queue *q);
+
+#endif
